0bc358a1c9b1cc1f708f5f1130553c66b0a37380
ayano63 <wunsch.aya@gmail.com>
2025-07-22 20:28:38 +0900

    DOM操作の課題upload

---
+++ kadai_020/event.js
@@ -0,0 +1,9 @@
+
+const addBtn = document.getElementById('btn');
+const textElement = document.getElementById('text');
+
+addBtn.addEventListener('click', () => {
+　textElement.textContent = 'ボタンをクリックしました';
+});
+
+
